86e375956f38de24e8000ea2fd06aa7b6e6f74f9
NayrL <40843838+NayrL114@users.noreply.github.com>
2022-09-19 15:02:01 +1000

    70 percent complete

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -7,6 +7,7 @@ public class CharacterMovement : MonoBehaviour
 
     private Vector3 movement;
     private float movementSqrMagnitude;
+    public float walkSpeed = 1.75f;
 
     // Update is called once per frame
     void Update()
@@ -22,17 +23,21 @@ public class CharacterMovement : MonoBehaviour
     {
         movement.x = Input.GetAxis("Horizontal");
         movement.z = Input.GetAxis("Vertical");
+        movement = Vector3.ClampMagnitude(movement, 1.0f);
         movementSqrMagnitude = movement.sqrMagnitude;
         Debug.Log(movement);
     }
 
     void CharacterPosition()
     {
-
+        movement.x = movement.x * walkSpeed * Time.deltaTime;
+        movement.z = movement.z * walkSpeed * Time.deltaTime;
+        transform.Translate(movement, Space.World);
     }
 
     void CharacterRotation()
     {
+        if (movement.x != 0 || movement.z != 0) transform.rotation = Quaternion.LookRotation(movement);
 
     }
 
